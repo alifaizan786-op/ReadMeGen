eabd3dc9dcaae917446a5cc0063569dbe0a75dae
alifaizan786-op <faizan.ali@irunadigital.com>
2021-11-02 01:05:04 -0400

    final fixed

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
+# The ReadMe below was generated using this readme generator
+## ReadMe Generator
+### Description
+This ReadMe generator is a CLI based application, it is based off of node.js. It provide you with a very ellaborative readMe just by answering a few questions
 
-    
+Live Link: [ReadMe Generator](https://github.com/alifaizan786-op/ReadMeGen)
+---
+### Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+---
+### Installation
+- clone the repo to your computer
+-  run the command "npm i"
+-  run the comand "index.js"
+-  answer the prompts
+-  copy the generated readme me to your repo
+
+### Usage
+Used to create readme for your repo
+
+![https://res.cloudinary.com/practicaldev/image/fetch/s--Yq5yTPVt--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://user-images.githubusercontent.com/9840435/59461914-cbc18380-8e22-11e9-8567-87b43da950ac.png](https://res.cloudinary.com/practicaldev/image/fetch/s--Yq5yTPVt--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://user-images.githubusercontent.com/9840435/59461914-cbc18380-8e22-11e9-8567-87b43da950ac.png)
+### Tests
+
+### Credits
+#### People
+- https://nodejs.org/en/docs/
+-  https://developer.mozilla.org/en-US/
+
+#### Resources
+- 
+
+### License
+![Apache](https://img.shields.io/static/v1?label=license&message=Apache&color=brightgreen&style=plastic)
+### Questions
+Github UN: alifaizan786-op - [alifaizan786-op](https://github.com/alifaizan786-op)
+You can contact me via email by emailing faizan.ali@irunadigital.com with your questions
+        
     
--- index.js
+++ index.js
@@ -65,40 +65,45 @@ inquirer
         type: 'input',
         message: "What's the link to the image you would like to add to the readme?",
         name: 'imgLink',
+    },
+    {
+        type: 'input',
+        message: "What's are the tests that you used?",
+        name: 'test',
     }
   ])
   .then(Response => {
     fs.writeFile('Readme.md',
     `
-    # ${projectName}
-    ## Description
-    ${projectDesc}\n
-    Live Link: [${projectName}](https://github.com/${githubUserN}/${projectName})
-    ---
-    ## Table of Contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [License](#license)
-    - [Questions](#questions)
-    ---
-    ## Installation
-    ${parseString(installation)}
-    ## Usage
-    ${projectUsage}\n
-    ![${imgLink}](${imgLink})
-    ## Tests
-    ${test}
-    ## Credits
-    ### People
-    ${parseString(projectCont)}
-    ### Resources
-    ${parseString(resources)}
-    ## License
-    ![${license}](https://img.shields.io/static/v1?label=license&message=${license}&color=brightgreen&style=plastic)
-    ## Questions
-    Github UN: ${githubUserN} - [${githubUserN}](https://github.com/${githubUserN})
-    You can contact me via email by emailing ${email} with your questions
-        
+# ${Response.projectName}
+## Description
+${Response.projectDesc}\n
+Live Link: [${Response.projectName}](https://github.com/${Response.githubUserN}/${Response.projectName})
+---
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+---
+## Installation
+${parseString(Response.installation)}
+## Usage
+${Response.projectUsage}\n
+![${Response.imgLink}](${Response.imgLink})
+## Tests
+${Response.test}
+## Credits
+### People
+${parseString(Response.projectCont)}
+### Resources
+${parseString(Response.resources)}
+## License
+![${Response.license}](https://img.shields.io/static/v1?label=license&message=${Response.license}&color=brightgreen&style=plastic)
+## Questions
+Github UN: ${Response.githubUserN} - [${Response.githubUserN}](https://github.com/${Response.githubUserN})
+You can contact me via email by emailing ${Response.email} with your questions
+    
     `, (err) => err ? console.log(err) : console.log('Success!'));
 }) 
